a0250bca9eb3ec3a237e3dd88559230d5d922f18
Lukas Bombach <lukas.bombach@googlemail.com>
2019-06-14 00:01:24 +0200

    wip

--- lib/characteristics/version.ts
+++ lib/characteristics/version.ts
@@ -1,4 +1,19 @@
 import binary from "binary";
+import { BinaryDesc } from "./binaryParser";
+
+// prettier-ignore
+export const binaryDesc: BinaryDesc[] = [
+  {name: "bluetooth.apiVersion", type:"word8u"},
+  {name: "bluetooth.release", type:"word8u"},
+  {name: "bluetooth.commits", type:"word16bu"},
+  {name: "bluetooth.changes", type:"word8u"},
+  {name: "bluetooth.sha", type:"word32lu", process: convertToHex},
+  {name: "firmware.apiVersion", type:"word8u"},
+  {name: "firmware.release", type:"word8u"},
+  {name: "firmware.commits", type:"word16bu"},
+  {name: "firmware.changes", type:"word8u"},
+  {name: "firmware.sha", type:"word32lu", process: convertToHex},
+]
 
 export interface Versions {
   bluetooth: Version;
