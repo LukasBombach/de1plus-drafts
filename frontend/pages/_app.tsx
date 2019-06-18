ab8c4ef6cfbd6b4adc799082e8f77297e157bb62
Lukas Bombach <lukas.bombach@googlemail.com>
2019-06-18 11:08:30 +0200

    refactoring

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,18 +1,8 @@
 import React from "react";
 import { ApolloProvider } from "react-apollo";
 import App, { Container } from "next/app";
-import { Layout } from "antd";
 import withApolloClient from "../lib/with-apollo-client";
-import Navigation from "../components/navigation";
 import "antd/dist/antd.css";
-
-const { Content } = Layout;
-
-const menuHeight = 64;
-const contentStyles = {
-  height: `calc(100vh - ${menuHeight}px)`,
-  overflow: "auto"
-};
 
 class MyApp extends App {
   render() {
@@ -20,12 +10,7 @@ class MyApp extends App {
     return (
       <Container>
         <ApolloProvider client={apolloClient}>
-          <Layout>
-            <Content style={contentStyles}>
-              <Component {...pageProps} />
-            </Content>
-            <Navigation height={menuHeight} />
-          </Layout>
+          <Component {...pageProps} />
         </ApolloProvider>
       </Container>
     );
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,1 +1,26 @@
-export default () => null;
+import { Layout } from "antd";
+import Navigation from "../components/navigation";
+const { Content } = Layout;
+
+export interface IndexPageProps {
+  menuHeight: number;
+}
+
+const IndexPage: React.FunctionComponent<IndexPageProps> = ({
+  children,
+  menuHeight = 96
+}) => {
+  const contentStyles = {
+    height: `calc(100vh - ${menuHeight}px)`,
+    overflow: "auto"
+  };
+
+  return (
+    <Layout>
+      <Content style={contentStyles}>{children}</Content>
+      <Navigation height={menuHeight} />
+    </Layout>
+  );
+};
+
+export default IndexPage;
