281e0b01705efde695250f7650e39a4ac9e0b710
Lukas Bombach <lukas.bombach@googlemail.com>
2019-06-22 15:04:18 +0200

    fix

--- backend/de1/v2/noble.ts
+++ backend/de1/v2/noble.ts
@@ -6,11 +6,11 @@ const connect = (peripheral: Peripheral) => promisify<void>(peripheral.connect);
 const disconnect = (peripheral: Peripheral) => promisify<void>(peripheral.disconnect);
 
 export default class Noble {
-  public static async connect(peripheral: Peripheral, ms: number): Promise<void> {
+  public static async connect(peripheral: Peripheral, ms?: number): Promise<void> {
     return Promise.race([connect(peripheral), timeout("connect", ms)]) as Promise<void>;
   }
 
-  public static async disconnect(peripheral: Peripheral, ms: number): Promise<void> {
+  public static async disconnect(peripheral: Peripheral, ms?: number): Promise<void> {
     return Promise.race([disconnect(peripheral), timeout("disconnect", ms)]) as Promise<void>;
   }
 }
